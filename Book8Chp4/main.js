2a6ed3ee759a252f452a6f9bb866407c1b726f07
Lauden Eller <laudlauden@gmail.com>
2022-01-31 13:51:39 -0800

    wrote some steps for chp 4

---
+++ Book8Chp4/main.js
@@ -0,0 +1,19 @@
+// Create a grade array of numbers
+
+// Create function that accepts an array of numbers
+
+    // Create variable with empty array
+
+    // Iterate the grade array with a for statement
+
+        // Check if value of grade is less than 38
+            
+            // Push grade to empty array
+
+        // Otherwise check if difference between grade and next multiple of 5 is less than 3
+            // Round the grade up to next multiple of 5
+            // Push rounded grade to empty array
+
+    // return an array of rounded grades
+
+// Print function results
